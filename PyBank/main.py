1d24ca7e371537c5cd813f2c20d633a1f827c7dc
Vincent Heningburg <GabrielUSMC@gmail.com>
2020-03-09 09:55:12 -0700

    Finished PyBank

---
+++ PyBank/main.py
@@ -0,0 +1,52 @@
+# This will allow us to create file paths across operating systems
+import os
+
+# Module for reading CSV files
+import csv
+
+csvpath = os.path.join('Resources', 'budget_data.csv')
+
+# Initialize Variables
+TotalMonths = 0
+NetPL = 0.0
+G_Inc = -1000.0
+G_Inc_Name = ""
+G_Dec = 1000.0
+G_Dec_Name = ""
+
+#Open cvs file and analyze
+with open(csvpath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # Read the header row first
+    csv_header = next(csvreader)
+
+    # Read each row of data after the header
+    for row in csvreader:
+        TotalMonths += 1
+        compare = float(row[1])
+        NetPL += compare
+        if compare > G_Inc:
+            G_Inc = compare
+            G_Inc_Name = row[0]
+        if compare < G_Dec:
+            G_Dec = compare
+            G_Dec_Name = row[0]
+
+with open("./output.txt",'w') as textfile:
+    textfile.write("Financial Analysis\n")
+    print("Financial Analysis")
+    textfile.write("----------------------------\n")
+    print("----------------------------")
+    textfile.write(f"Total Months: {TotalMonths}\n")
+    print(f"Total Months: {TotalMonths}")
+    textfile.write(f"Total: ${NetPL}\n")
+    print(f"Total: ${NetPL}")
+    textfile.write(f"Average  Change: ${NetPL/TotalMonths}\n")
+    print(f"Average  Change: ${NetPL/TotalMonths}")
+    textfile.write(f"Greatest Increase in Profits: {G_Inc_Name} (${G_Inc})\n")
+    print(f"Greatest Increase in Profits: {G_Inc_Name} (${G_Inc})")
+    textfile.write(f"Greatest Decrease in Profits: {G_Dec_Name} (${G_Dec})\n")
+    print(f"Greatest Decrease in Profits: {G_Dec_Name} (${G_Dec})")
